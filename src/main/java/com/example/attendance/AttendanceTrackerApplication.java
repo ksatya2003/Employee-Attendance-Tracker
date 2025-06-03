3bec5d133d0db47a80393238dd81023e593c7f06
Lahari Kantamaneni <110590529+ksatya2003@users.noreply.github.com>
2025-06-03 13:58:31 +0530

    Create AttendanceTrackerApplication.java

---
+++ src/main/java/com/example/attendance/AttendanceTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.example.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceTrackerApplication.class, args);
+    }
+}
